0a1d62b2c8190ed04002298078d9b580f16f9e0a
rockgod100 <jon@ancillaryfactory.com>
2012-09-01 22:06:59 -0500

    Setup now adds install and uninstall files

--- setup.bat
+++ setup.bat
@@ -1,5 +1,9 @@
 mkdir "c:\Program Files\Webname"
 copy "webname.exe" "c:\Program Files\Webname"
+copy "setup.bat" "c:\Program Files\Webname"
+copy "uninstall.bat" "c:\Program Files\Webname"
+copy "readme.txt" "c:\Program Files\Webname"
+
 
 REG ADD "HKCR\*\shell\Rename for Web"
 REG ADD "HKCR\*\shell\Rename for Web\command" /d "C:\Program Files\Webname\webname.exe \"%%1\""
